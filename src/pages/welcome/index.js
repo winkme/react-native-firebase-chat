67edebd50eb727382596577fe45efff2bc187ba0
José Roberto Emerich Junior <jremerich@gmail.com>
2018-08-02 21:37:50 -0300

    capturando informacoes da conexao wifi

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Geolocation from 'react-native-geolocation-service';
 import Permissions from 'react-native-permissions';
+import { NetworkInfo } from 'react-native-network-info';
 import {
     View,
     Text,
@@ -17,15 +18,30 @@ export default class Welcome extends Component {
 
     state = {
         position: 'aguardando...',
+        wifi: {},
     };
 
     componentDidMount() {
         this.setState({ position: 'iniciando busca...' });
         this.gpsInit();
+
+        console.tron.log('NetworkInfo: ', NetworkInfo);
+
+        NetworkInfo.getSSID((ssid) => {
+            console.tron.log('ssid: ', ssid);
+            let wifi = this.state.wifi;
+            wifi.ssid = ssid;
+            this.setState({ wifi });
+        });
+        NetworkInfo.getBSSID((bssid) => {
+            console.tron.log('bssid: ', bssid);
+            let wifi = this.state.wifi;
+            wifi.bssid = bssid;
+            this.setState({ wifi });
+        });
     }
 
     gpsInit = async () => {
-        console.tron.log('Permissions', Permissions);
         Permissions.request('location').then((response) => {
             if (response === 'authorized') {
                 Geolocation.watchPosition((position) => {
@@ -34,7 +50,7 @@ export default class Welcome extends Component {
                 }, (error) => {
                     // See error code charts below.
                     console.tron.log(error.code, error.message);
-                }, { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000, distanceFilter: 0 });
+                }, { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000, distanceFilter: 50 });
             } else {
                 console.tron.log("GPS permission denied");
             }
@@ -69,6 +85,10 @@ export default class Welcome extends Component {
 
                 <Text style={styles.text}>
                     {this.state.position.coords ? `${this.state.position.coords.latitude}, ${this.state.position.coords.longitude}` : 'buscando gps...'}
+                </Text>
+
+                <Text style={styles.text}>
+                    {JSON.stringify(this.state.wifi)}
                 </Text>
             </View>
         );
